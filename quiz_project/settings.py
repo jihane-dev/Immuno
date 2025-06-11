9ff78805dc249b9bea7748b65c2e58f110b19465
jihane-dev <jihane.jait@uit.ac.ma>
2025-06-11 21:08:18 +0200

    Corrige urls.py et settings.py

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'quizz_app',
+
     
 ]
 
@@ -73,7 +75,7 @@ TEMPLATES = [
         },
     },
 ]
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'quiz_app/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'quizz_app/static')]
 
 
 WSGI_APPLICATION = 'quiz_project.wsgi.application'
@@ -82,12 +84,20 @@ WSGI_APPLICATION = 'quiz_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ['DATABASE_URL'], conn_max_age=600, ssl_require=True)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+
 
 
 # Password validation
